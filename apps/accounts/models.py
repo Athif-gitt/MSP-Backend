672e641dd64f9f460f3030d7067b6a08ea85c21b
Athif-gitt <athiffaisal55@gmail.com>
2026-02-27 12:02:02 +0530

    test

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
 
+
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email must be provided")
