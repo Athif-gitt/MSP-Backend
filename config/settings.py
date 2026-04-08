be8d55e330cf4c959aa7f07ef9b2e7cdb0aa2fe1
Athif-gitt <athiffaisal55@gmail.com>
2026-04-08 11:15:21 +0530

    changed to RDS

--- config/settings.py
+++ config/settings.py
@@ -122,13 +122,13 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'msp_db',
-        'USER': 'msp_user',
-        'PASSWORD': 'Msp@2468',
-        'HOST': 'db',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
